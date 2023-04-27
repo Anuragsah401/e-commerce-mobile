55d432f83faaeff4d5c10857589ad2b7761fd626
Anurag <anuragsah401@gmail.com>
2023-04-27 16:07:52 +0545

    learning react native

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = () => {
+  return (
+    <View>
+      <Text>ScreenHeaderBtn asdas</Text>
+    </View>
+  )
+}
+
+export default ScreenHeaderBtn
